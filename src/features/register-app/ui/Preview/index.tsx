9edca8ccdd27b4bf3ff45da0035d6d50b83fc609
김태우 <taewoo080401@gmail.com>
2026-03-10 16:34:26 +0900

    feat: 세그먼트 버튼으로 변경

--- src/features/register-app/ui/Preview/index.tsx
+++ src/features/register-app/ui/Preview/index.tsx
@@ -1,23 +1,38 @@
 import EntryScreen from "@/features/register-app/ui/EntryScreen";
 import { Column } from "@/shared/styles/common";
-import { Tab } from "@b1nd/dodam-design-system";
+import {
+  SegmentedButton,
+  type SegmentedButtonData,
+} from "@b1nd/dodam-design-system";
 import * as S from "./style";
 import { useState } from "react";
 import PushScreen from "@/features/register-app/ui/PushScreen";
 
-// TODO: 세그먼트로 바꾸기
-
 const Preview = () => {
-  const [screen, setScreen] = useState(0);
+  const [screen, setScreen] = useState<SegmentedButtonData[]>([
+    {
+      text: "진입 화면",
+      value: "entry-screen",
+      isActive: true,
+    },
+    {
+      text: "푸시 알림",
+      value: "push-screen",
+      isActive: false,
+    },
+  ]);
+  const [selected, setSelected] = useState(screen[0].value);
 
   return (
     <S.Container>
       <Column $gap={16}>
-        <Tab onChange={setScreen}>
-          <Tab.Item selected={screen === 0}>진입 화면</Tab.Item>
-          <Tab.Item selected={screen === 1}>푸시 알림</Tab.Item>
-        </Tab>
-        {screen ? <PushScreen /> : <EntryScreen />}
+        <SegmentedButton
+          data={screen}
+          setData={setScreen}
+          onBlockClick={setSelected}
+          containerCustomStyle={{ width: "100%" }}
+        />
+        {selected === "push-screen" ? <PushScreen /> : <EntryScreen />}
       </Column>
     </S.Container>
   );
