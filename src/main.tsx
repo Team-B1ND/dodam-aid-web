97e705568c50cb5afff4c45713a0ed9806063b0a
dlskl <skyandme77@gmail.com>
2026-06-13 22:32:28 +0900

    feat: HelmetProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
 import GlobalStyle from "@/shared/styles/GlobalStyle";
 import Router from "@/shared/routes";
 import "@b1nd/dodam-design-system/colors/colors.css";
@@ -8,11 +9,13 @@ import { OverlayProvider, ToastProvider } from "@b1nd/dodam-design-system";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <ToastProvider />
-    <OverlayProvider>
-      <GlobalStyle />
-      <Router />
-    </OverlayProvider>
-  </QueryClientProvider>,
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider />
+      <OverlayProvider>
+        <GlobalStyle />
+        <Router />
+      </OverlayProvider>
+    </QueryClientProvider>
+  </HelmetProvider>,
 );
